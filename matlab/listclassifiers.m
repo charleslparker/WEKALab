6f6c7e8284aee311373c1aa7f185c2b4e515de31
Charles Parker <clp@clparker.org>
2012-03-03 15:53:07 -0800

    Removing old source files

--- matlab/listclassifiers.m
+++
@@ -1,7 +0,0 @@
-function listclassifiers()
-    %LISTCLASSFIERS lists the classifiers available in WekaLab
-    %   LISTCLASSIFIERS shows the defined constants available for use as
-    %   the 'type' paramater in TRAINANDTEST, CROSSVALIDATE, and
-    %   TRAINCLASSIFIER.  These are string constants.
-    org.clparker.wekalab.WekaInterface.listClassifiers();
-end
--- matlab/readarfffile.m
+++
@@ -1,8 +0,0 @@
-function data = readarfffile(name)
-    %READARFFFILE reads a file in ARFF format
-    %   READARFFFILE('filename') reads the file given in name into a 
-    %   matrix of doubles. The file must be in ARFF format.  For more about 
-    %   ARFF, visit the WEKA home page.
-    in = org.clparker.wekalab.WekaInterface.readArffFile(name);
-    data = org.clparker.wekalab.WekaInterface.setToDouble(in);
-end
