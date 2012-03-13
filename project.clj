c4ddfee249748a61b674a96c43c0603b18052f79
Charles Parker <clp@clparker.org>
2012-03-12 19:38:59 -0700

    Adding uberjar name

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject org.clparker/wekalab "0.3.0-SNAPSHOT"
   :description "Extremely simple interface from WEKA to MATLAB"
   :java-source-path "src/java"
+  :javac-options {:source "1.6" :target "1.6" :bootclasspath "jdk1.6.0/lib/rt.jar"}
+  :uberjar-name "wekalab.jar"
   :dependencies [[weka "3.6.2"]])
